ac2214025bf61c3cbdb11a4f67e27b91f8dc3776
agonzale <agonzale@mathworks.com>
2023-06-25 16:32:47 +0100

    fix: minor issue in tests tGetPageScoreForKeyword.m

--- tests/tools/tGetPageScoreForKeyword.m
+++ tests/tools/tGetPageScoreForKeyword.m
@@ -14,30 +14,34 @@ classdef tGetPageScoreForKeyword < matlab.unittest.TestCase
 
         function testOutput(test, page, keyword)
             
-            score = test.funcUnderTest(page,keyword);
+            engine = hSearchEngineMockup("GoogleEngineSearchSample1_CellItems.mat");
+            score = test.funcUnderTest(page,keyword,'engine',engine);
             test.verifyLength(score,1);
             test.verifyTrue(isnumeric(score), "Score is not numeric")
 
         end
 
         function testOutputIsReasonable(test)
+            engine = hSearchEngineMockup("GoogleEngineSearchSample1_CellItems.mat");
             matlabDocPage = "https://www.mathworks.com/help/matlab/";
-            matlabScore = test.funcUnderTest(matlabDocPage,"matlab");
-            potatoScore = test.funcUnderTest(matlabDocPage,"potato");
+            matlabScore = test.funcUnderTest(matlabDocPage,"matlab",'engine',engine);
+            potatoScore = test.funcUnderTest(matlabDocPage,"potato",'engine',engine);
             test.verifyGreaterThan(matlabScore,potatoScore);
         end
 
         function testOutputIsNormalized(test, page, keyword)
-            score = test.funcUnderTest(page,keyword);
+            engine = hSearchEngineMockup("GoogleEngineSearchSample1_CellItems.mat");
+            score = test.funcUnderTest(page,keyword,'engine',engine);
             test.verifyGreaterThanOrEqual(score,0);
             test.verifyLessThanOrEqual(score,1);
         end
 
         function testOutput_Negative(test)
             
+            engine = hSearchEngineMockup("GoogleEngineSearchSample1_CellItems.mat");
             invalidPage = "<dummypage>";
             aKeyword = "hola";
-            test.verifyError(@() test.funcUnderTest(invalidPage,aKeyword), ...
+            test.verifyError(@() test.funcUnderTest(invalidPage,aKeyword,'engine',engine), ...
                 'MATLAB:webservices:ExpectedProtocol')
             
         end
@@ -55,10 +59,10 @@ classdef tGetPageScoreForKeyword < matlab.unittest.TestCase
         end
 
         function testMethodOption_Negative(test)
-
+            engine = hSearchEngineMockup("GoogleEngineSearchSample1_CellItems.mat");
             invalidMethod = "<dummyMethod>";
             test.verifyError(@() test.funcUnderTest(test.page{1}, test.keyword{1}, ...
-                 method=invalidMethod), ...
+                 method=invalidMethod, engine=engine), ...
                 'getPageScoreForKeyword:invalidMethod');
             
         end
